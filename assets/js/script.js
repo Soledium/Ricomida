49760b6e966d73389fc545b883512324cdc60bd8
Soledium <nescobar3@hotmail.com>
2022-05-31 17:13:29 -0400

    Agregando Desafío Ricomida con JS

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+// alerta del mail
+$( "#pop-up" ).click(function() {
+    alert( "El correo fue enviado correctamente..." );
+ });
+//  "INGREDIENTES" y "PREPARACIÓN" a color rojo
+ var listadodbl = $("#listado").first();
+ listadodbl.dblclick(function(){
+    listadodbl.toggleClass("dbl")
+ })
+ var elaboraciondbl = $("#elaboracion").first();
+ elaboraciondbl.dblclick(function(){
+     elaboraciondbl.toggleClass("dbl")
+ })
+//  desaparecer y aparecer el contenido en todas las tarjetas "card" de la sección de Recetas Relacionadas
+$( ".card-title" ).click(function(){
+    $(".card-text").toggle(function() {
+    });
+})
+
+
