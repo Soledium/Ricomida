ce29e11889c227701f4ab4b738cd19ac7e7ed82a
Soledium <nescobar3@hotmail.com>
2022-06-04 19:55:11 -0400

    Agregando final desafío

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
 
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
 // alerta del mail
 $( "#pop-up" ).click(function() {
     alert( "El correo fue enviado correctamente..." );
